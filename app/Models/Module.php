3f7091c6dfb8a1f5d86713b0a40c86600c49ebc2
Eavila4906 <eavila4906@gmail.com>
2024-06-22 15:37:14 -0500

    Hierarchy is added to the modules, implementing submodules and elements.

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    protected $fillable = ['module', 'description', 'status'];
+    protected $fillable = [
+        'module', 
+        'path', 
+        'description',
+        'icon', 
+        'status'
+    ];
     protected $primaryKey = 'id';
+
+    public function submodules()
+    {
+        return $this->hasMany(Submodule::class);
+    }
 }
